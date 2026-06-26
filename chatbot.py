72d36418f26ea14def339dc43d95af40d2044228
ValenMontes96 <valentinmontes96@gmail.com>
2026-06-26 20:00:29 -0300

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,52 @@
+import os
+import google.generativeai as genai
+from dotenv import load_dotenv
+
+#Configurar la API KEY
+load_dotenv()
+#Definir el System Prompt 
+
+# Obtener la API Key de forma segura
+API_KEY = os.getenv("GEMINI_API_KEY")
+
+if not API_KEY:
+    print("Error: No se encontró la API_KEY. Verifica tu archivo .env")
+    exit()
+
+genai.configure(api_key=API_KEY)
+
+instrucciones_sistema = (
+    "Eres un tutor experto en programación de sistemas. "
+    "Responde siempre en español. Tus respuestas deben ser  "
+    "directas y cordiales. Si te preguntan algo que no sea de tecnología "
+    "o programación, indica amablemente que tu enfoque es exclusivamente técnico."
+)
+
+#Configurar el modelo y sus parámetros
+modelo = genai.GenerativeModel(
+    model_name='gemini-2.5-flash', 
+    system_instruction=instrucciones_sistema
+)
+
+
+chat = modelo.start_chat(history=[])
+
+
+while True:
+    
+    entrada_usuario = input("\n Usuario: ")
+
+    
+    if entrada_usuario.lower() in ['salir', 'exit', 'quit']:
+        print("\n Asistente: ¡Hasta luego! Éxitos con tu código.")
+        break
+
+    try:
+        
+        respuesta = chat.send_message(entrada_usuario)
+        
+        
+        print(f" Asistente: {respuesta.text}")
+        
+    except Exception as e:
+        print(f"\n Error al comunicarse con la API: {e}")
